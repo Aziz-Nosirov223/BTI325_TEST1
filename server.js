972fd62bbf19204f925989087935a1be5a0a3779
Aziz Nosirov <anosirov@myseneca.ca>
2022-10-05 18:06:06 -0400

    updated server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/", function(req, res) {
 
 app.get("/test1", function(req, res) {
     var text2 = "<h1>Good luck!</h1><br>";
-    tex2t += "<a href=''>Go Home</a>";
+    text2 += "<a href=''>Go Home</a>";
     res.send(text2);
   
 
