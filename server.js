71002ea497fd9875126df29c29dda4e66a3696d7
Aziz Nosirov <anosirov@myseneca.ca>
2022-10-05 18:14:25 -0400

    updated server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get("/", function(req, res) {
 app.get("/test1", function(req, res) {
     var text2 = "<h1>Good luck!</h1>";
     text2 += "<a href='./'>Go Home</a>";
+    text2.backgroundColor = "red"
     res.send(text2);
   
 
